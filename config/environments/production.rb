174184401b37a76a4df42097dbd2319ba13eea09
Wasim Akram <mailwasim@gmail.com>
2012-11-21 21:10:38 +0500

    Heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Bundlemoney::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  #Heroku requirenment
-  config.assets.initialize_on_precompile = false
 
   # Code is not reloaded between requests
   config.cache_classes = true
